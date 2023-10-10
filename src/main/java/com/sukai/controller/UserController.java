8573002e2da57d145cde2f618f8232fd1553ac5f
chengsukai <chengsukai@gmail.com>
2023-10-10 14:37:19 +0800

    添加@ResponseBody,防止裸调接口404

--- src/main/java/com/sukai/controller/UserController.java
+++ src/main/java/com/sukai/controller/UserController.java
@@ -3,20 +3,18 @@ package com.sukai.controller;
 import com.sukai.dto.UserVo;
 import com.sukai.entity.User;
 import com.sukai.service.UserService;
+import com.sukai.utils.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author chengsukai
  */
 @Controller
-@RequestMapping("/user")
+@RequestMapping
 public class UserController {
 
     @Autowired
@@ -30,12 +28,10 @@ public class UserController {
         return user;
     }
 
-    @RequestMapping(value = "/getAllUser",method = RequestMethod.GET)
-    public ModelAndView getAllUser() {
-        ModelAndView modelAndView = new ModelAndView();
+    @RequestMapping(value = "/getAllUser", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseResult getAllUser() {
         User user = userService.findByUserName("admin");
-        modelAndView.addObject("user", user);
-        modelAndView.setViewName("index");
-        return modelAndView;
+        return new ResponseResult<User>(200, "操作成功", user);
     }
 }
